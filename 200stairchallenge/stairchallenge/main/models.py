efe099ea01f5b4e672d8d78ff21c05242c736379
Jon Prettyman <jon.kellyst@me.com>
2009-07-07 21:19:50 -0700

    removed old stairchallenge sample

--- 200stairchallenge/stairchallenge/main/models.py
+++
@@ -1,8 +0,0 @@
-# models.py
-
-from google.appengine.ext import db
-
-class Visitor(db.Model):
-    ip = db.StringProperty()
-    added_on = db.DateTimeProperty(auto_now_add=True)
-
--- 200stairchallenge/stairchallenge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    (r'^$', 'stairchallenge.main.views.main'),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
-)
